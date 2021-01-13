acc744356e843031822bb603d355a514b3778f98
Ankit Goyal <52366781+ankit80570@users.noreply.github.com>
2021-01-13 13:45:02 +0530

    Update twilio_credentials.py

--- twilio_credentials.py
+++ twilio_credentials.py
@@ -1,4 +1,4 @@
-cellphone = "+917840038110"
-twilio_number = "+18329051670"
-twilio_account = "ACcb6e2e526aedcec0f1dfd2438ddbd486"
-twilio_token = "21dd54be39fd8a759993dc3c9a68f6a0"
+cellphone = "+91784XXXXXXX"
+twilio_number = "+1832XXXX"
+twilio_account = "AXXXXXXXXXXXXXXXXXXX"
+twilio_token = "21dXXXXXXXXXXXXXXXXXXX"
